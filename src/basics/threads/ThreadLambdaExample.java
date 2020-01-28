f308bf3db57bec6e04259eef8f714c6f14cfbdaf
egnaf <egnaf@yahoo.com>
2020-01-28 13:43:52 +0300

    Add an example class to create threads using lambda

---
+++ src/basics/threads/ThreadLambdaExample.java
@@ -0,0 +1,20 @@
+package basics.threads;
+
+public class ThreadLambdaExample {
+
+    public static void main(String[] args) {
+
+        // #1
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread());
+            }
+        }).start();
+
+        // #2 with lambda
+        new Thread(() -> {
+            System.out.println(Thread.currentThread());
+        }).start();
+    }
+}
