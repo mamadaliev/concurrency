fb16606893d5dadb236eba64b2eca15d2326cdb9
egnaf <egnaf@yahoo.com>
2020-01-29 10:25:27 +0300

    Add executor service class example

---
+++ src/concurreny/ExecutorServiceExample.java
@@ -0,0 +1,15 @@
+package concurreny;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        executor.submit(() -> {
+            System.out.println("Hello, " + Thread.currentThread().getName()); // out: Hello, pool-1-thread-1
+        });
+    }
+}
