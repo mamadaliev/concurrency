d29db3831c801f3d0579d68e12b9992ce6e6a125
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-05-16 19:47:46 +0300

    Add volatile example

---
+++ src/votatile/A.java
@@ -0,0 +1,24 @@
+package votatile;
+
+public class A<T> {
+
+    // 1. The volatile modifier allows not to cache a variable in threads.
+    // 2. Provides atomic reading in threads.
+    private volatile T data;
+
+    public T getData() {
+        return this.data;
+    }
+
+
+    public static void main(String[] args) {
+        A<Integer> a = new A<>();
+
+        // Atomic reading
+        System.out.println(a.getData());
+
+        // Non-atomic modification.
+        // You need to increment/decrement variable use Atomics from Concurrency.
+        a.data++;
+    }
+}
