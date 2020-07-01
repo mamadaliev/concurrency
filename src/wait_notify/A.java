3b8b8b3b640eb00199401254e0e3c33337d9adec
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-07-01 20:42:28 +0300

    Fix provider consumer synchronize problem solution

--- src/wait_notify/A.java
+++ src/wait_notify/A.java
@@ -14,16 +14,23 @@ public class A<E> {
         data = new PriorityQueue<>();
     }
 
-    public synchronized void provide(E element) {
-        data.offer(element);
-        this.notifyAll();
+    public synchronized void provide(E element) throws InterruptedException {
+        if (data.offer(element)) {
+            System.out.println("Provided: " + data.size());
+            this.notifyAll();
+        } else {
+            System.err.println("An element is not provided.");
+        }
+        Thread.sleep(300);
     }
 
     public synchronized void consume() throws InterruptedException {
         while (data.size() <= 0)
             wait();
 
+        Thread.sleep(100);
         data.remove();
+        System.err.println("Consumed: " + data.size());
         this.notifyAll();
     }
 
@@ -32,20 +39,35 @@ public class A<E> {
 
         // The first thread which call the provide method of A class and send number to queue.
         new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                a.provide(i);
+            for (int i = 0; i < 5; i++) {
+                try {
+                    a.provide(i);
+                } catch (InterruptedException ignore) { }
             }
         }).start();
 
         // The second thread which call the consume method of A class for remove number from queue.
         new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 5; i++) {
                 try {
                     a.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                } catch (InterruptedException ignore) { }
             }
         }).start();
     }
 }
+
+/*
+    Output:
+
+    Provided: 1
+    Provided: 1
+    Consumed: 0
+    Provided: 2
+    Consumed: 1
+    Consumed: 0
+    Provided: 1
+    Consumed: 0
+    Provided: 1
+    Consumed: 0
+*/
