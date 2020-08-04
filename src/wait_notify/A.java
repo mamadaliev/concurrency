1c83197fdf48bd5958e1c42b5851ee97a45e08db
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-08-04 21:57:20 +0300

    Change wait and notify example description

--- src/wait_notify/A.java
+++ src/wait_notify/A.java
@@ -4,7 +4,7 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /*
-    Provider-Consumer Synchronize Problem
+    Provider-Consumer Synchronize Problem with wait() and notifyAll()
 */
 public class A<E> {
 
